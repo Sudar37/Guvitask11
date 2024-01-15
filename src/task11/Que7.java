3b91936e4b16844765300eff565d89ced4bad389
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-15 23:51:04 +0530

    Update Que7.java

--- src/task11/Que7.java
+++ src/task11/Que7.java
@@ -2,31 +2,36 @@ package task11;
 
 import java.util.Scanner;
 
-
 class InvalidAgeException extends Exception {
-    public InvalidAgeException(String errorMessage) {
-        super(errorMessage);
+public InvalidAgeException(String errorMessage) {
+super(errorMessage);
     }
 }
 
 public class Que7 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
 
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter your age: ");
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter your age: ");
         int age = sc.nextInt();
-       try {
-           if (age < 18) 
-            {
-                throw new InvalidAgeException("Age must be greater than or equal to 18.");
-            }
+        try {
+        if (age < 18) 
+        {
+            throw new InvalidAgeException("Age must be greater than or equal to 18.");
+        }
             System.out.println("Age is valid.");
         } catch (InvalidAgeException e) {
             System.out.println("Error: InvalidAgeException: " + e.getMessage());
        }
-	}
-
+    }
 }
+-----------------------------------------------------------------------------------------------------------------
+OUTPUT A:
+Enter your age: 12
+Error: InvalidAgeException: Age must be greater than or equal to 18.
 
+OUTPUT B:
+Enter your age: 22
+Age is valid.
