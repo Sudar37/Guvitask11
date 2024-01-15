6891e59ed4cce4b5d492ee3f512d900acd56f4b2
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-15 23:42:58 +0530

    Update Que4.java

--- src/task11/Que4.java
+++ src/task11/Que4.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 
 public class Que4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the value of A: ");
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the value of A: ");
         int A = sc.nextInt();
         
         System.out.print("Enter the value of B: ");
         int B = sc.nextInt();
 		
         System.out.print(A / B);
-	}
-
+   }
 }
+---------------------------------------------------------------------------------------------------------------------
+OUTPUT:
+Enter the value of A: 45
+Enter the value of B: 0
+Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at guvitask11/task11.Que4.main(Que4.java:17)
