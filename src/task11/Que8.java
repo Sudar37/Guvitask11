69e8afc052aba37fc04f0a58f3ec279c8742c2ab
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-15 23:52:30 +0530

    Update Que8.java

--- src/task11/Que8.java
+++ src/task11/Que8.java
@@ -4,28 +4,32 @@ import java.io.*;
 
 public class Que8 {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 
-		FileReader reader = new FileReader("file.txt"); 
+	FileReader reader = new FileReader("file.txt"); 
 
-		// passing FileReader to 
-		// buffered reader 
-		BufferedReader br = new BufferedReader(reader); 
+	// passing FileReader to 
+	// buffered reader 
+	BufferedReader br = new BufferedReader(reader); 
   
-		// declaring empty string  
-		String data =null; 
-  
-		// while loop to read data  
+	// declaring empty string  
+	String data =null; 
+  	// while loop to read data  
 		 
-		try {
-			while ((data = br.readLine()) != null)  
-			{ 
+	try {
+		while ((data = br.readLine()) != null)  
+		{ 
 				System.out.println(data); 
-			}
-		} catch (IOException e) {
+		}
+	} catch (IOException e) {
 	
-			e.printStackTrace();
-		} 
-  
-	}
+		e.printStackTrace();
+	} 
+    }
 }
+---------------------------------------------------------------------------------------------------------------------
+OUTPUT:
+Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+	Unhandled exception type FileNotFoundException
+
+	at guvitask11/task11.Que8.main(Que8.java:9)
