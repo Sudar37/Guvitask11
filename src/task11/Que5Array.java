6008e7bb136b21e7d398a1e1fc0b19318c3f06ad
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-15 23:44:17 +0530

    Update Que5Array.java

--- src/task11/Que5Array.java
+++ src/task11/Que5Array.java
@@ -2,16 +2,24 @@ package task11;
 
 public class Que5Array {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String[] arr = {"Rohit","Shikar","Virat","Dhoni"};   
-		//Declaring 4 elements in the String array                                       
+	String[] arr = {"Rohit","Shikar","Virat","Dhoni"};   
+	//Declaring 4 elements in the String array                                       
 		          
-		for(int i=0;i<=arr.length;i++) {       
+	for(int i=0;i<=arr.length;i++) {       
 		 
-		System.out.println(arr[i]);   //So it will throw ArrayIndexOutOfBoundException  
+	System.out.println(arr[i]);   //So it will throw ArrayIndexOutOfBoundException  
 		        
-	   }  
+	}  
     }
 }
+---------------------------------------------------------------------------------------------------------------------\
+OUTPUT:
+Rohit
+Shikar
+Virat
+Dhoni
+Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 4 out of bounds for length 4
+	at guvitask11/task11.Que5Array.main(Que5Array.java:13)
